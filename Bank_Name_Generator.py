e1cb15333cd2700b7ee0a75b26b0a2dfe77f666a
Siris4 <gavin.l.martin@gmail.com>
2023-12-20 16:33:09 -0600

    Day 1

---
+++ Bank_Name_Generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+users_name = input("Hello, what is your name? \n ")
+#2. Ask the user for the city that they grew up in.
+city = input("Thank you "+users_name+". Now please tell us the city you grew up in: \n ")
+#3. Ask the user for the name of a pet.
+pet = input("Tell us the name of a pet: \n ")
+#4. Combine the name of their city and pet and show them their band name.
+print("Well, "+users_name+ " your band name is: \n " + city + pet)
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name
